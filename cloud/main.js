31153ac5c5dea44dfc8ceefbed9f95437f361c26
Anthony <anthony.bernardo@geomatic.ch>
2016-01-23 00:04:47 +0100

    Create coupon after order save

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,40 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+/*
+ * Create coupon after save Order
+ */
+Parse.Cloud.afterSave("Order", function(request, response) {
+
+  var CouponClass = Parse.Object.extend("Coupon");
+  var coupon = new CouponClass();
+  coupon.set("couponId", generateId());
+  coupon.set("type", getRandomKey());
+  coupon.set("user", request.object.get("user"));
+  coupon.save(null,{
+    success:function() {
+      response.success();
+    },
+    error:function(error) {
+      response.error(error);
+    }
+  });
+
 });
+
+/*
+ * Get the type of coupon
+ */
+function getRandomKey() {
+  var random = Math.random();
+  if (random < 0.75) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+/*
+ * Generate coupon ID randomly
+ */
+function generateId(){
+  return Math.round(Math.random() * 100000000)
+}
+
