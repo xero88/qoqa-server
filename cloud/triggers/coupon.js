a58cb22eec7da16d6b74bf7a3143538e361022b8
anthony.bernardo <anthony.bernardo>
2016-01-26 09:52:13 +0100

    Generate coupon type

--- cloud/triggers/coupon.js
+++ cloud/triggers/coupon.js
@@ -7,8 +7,9 @@ Parse.Cloud.afterSave("Order", function(request, response) {
     var CouponClass = Parse.Object.extend("Coupon");
     var coupon = new CouponClass();
     coupon.set("couponId", generateId());
-    coupon.set("type", getRandomKey()); // TODO check if key platine exist, if yes, redo randomKey
+    coupon.set("type", getRandomKey());
     coupon.set("user", request.object.get("user"));
+    coupon.set("used", false);
     coupon.save(null,{
         success:function() {
             response.success();
@@ -22,16 +23,25 @@ Parse.Cloud.afterSave("Order", function(request, response) {
 
 /*
  * Get the type of coupon
+ *
+ * 1 = gold
+ * 2 = silver
+ * 3 = bronze
+ *
  */
 function getRandomKey() {
     var random = Math.random();
-    if (random < 0.95) {
-        return 1;
+    if (random < 0.75) {
+        return 3;
     }
-    else {
+    else if (random < 0.95) {
         return 2;
     }
+    else {
+        return 1;
+    }
 }
+
 /*
  * Generate coupon ID randomly
  */
